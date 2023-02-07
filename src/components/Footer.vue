a7f1f523b3dd827052270ef2695ac50fc2524d4d
Katarina <katarina.mursec@gmail.com>
2023-02-07 23:43:38 +0100

    cleanup, add header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        eventName: import.meta.env.VITE_APP_TITLE
+      }
+    }
+  }
+</script>
+
+<template>
+  <footer id="footer">
+    <div class="footer-item">{{ eventName }}</div>
+    <div class="footer-item">2023</div>
+  </footer>
+</template>
+
+<style scoped>
+  #footer {
+    width: 100%;
+    background-color: grey;
+    padding: 1rem;
+  }
+
+  .footer-item {
+    text-align: center;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        eventName: import.meta.env.VITE_APP_TITLE
+      }
+    }
+  }
+</script>
+
+<template>
+  <header id="header">
+    <div class="header-item">{{ eventName }}</div>
+    <div class="header-item">Event</div>
+    <div class="header-item">Location</div>
+  </header>
+</template>
+
+<style scoped>
+  #header {
+    display: flex;
+    width: 100%;
+    background-color: grey;
+  }
+  .header-item {
+    flex: 1 1 100px;
+    text-align: center;
+    font-size: 1.2rem;
+    font-weight: bold;
+    padding: 0.5rem;
+  }
+</style>
