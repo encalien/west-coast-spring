9383f7e150b0a5e86c683af5e9ea34430ed57db2
Katarina <katarina.mursec@gmail.com>
2023-02-10 14:05:39 +0100

    add googlemaps

--- src/components/pages/EventLocation.vue
+++ src/components/pages/EventLocation.vue
@@ -17,6 +17,8 @@ export default {
       <p>{{ $t('location.workshopsLocationAddress.country') }}</p>
     </address>
 
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2768.5550688276053!2d14.506221715189787!3d46.05997230151305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4765329bda01817d%3A0x3598d33068363f77!2sStudio%20Dansa!5e0!3m2!1ssl!2ssi!4v1676034152943!5m2!1ssl!2ssi" class="map" allow="fullscreen" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
     <p v-for="i in [0,1]">
       {{ $t(`location.hotelSuggestionsText[${i}]`) }}
     </p>
@@ -28,4 +30,9 @@ export default {
 </template>
 
 <style scoped>
+  .map {
+    width: 100%;
+    height: 300px;
+    border: none;
+  }
 </style>
