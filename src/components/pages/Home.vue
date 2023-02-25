c758ca6f8fc58a7e4746e8de200881546a828959
Katarina <katarina.mursec@gmail.com>
2023-02-25 20:34:51 +0100

    add registration button on home page

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -17,8 +17,8 @@ export default {
       <!-- <h1>{{ $t('event.name') }}</h1>
       <h2>{{ $t('event.location') }}</h2>
       <h2>{{ $t('event.date') }}</h2> -->
-      <h2 class="margin-0">{{ $t('event.welcomeText') }}<br>{{ $t('registration.opensSoonText') }}</h2>
-      <!-- <a href="#/registration" class="btn btn-primary">{{ $t('registration.pageTitle') }}</a> -->
+      <h2 class="margin-0">{{ $t('event.welcomeText') }}</h2>
+      <a href="#/registration" class="btn btn-primary">{{ $t('registration.pageTitle') }}</a>
     </div>
   </section>
   <section>
