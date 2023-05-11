bdb78d5b1d69fde9338603409a5e5b249440f359
Katarina Mursec <katarina.mursec@gmail.com>
2023-05-11 15:56:54 +0200

    add dark mode

--- src/components/pages/Pricing.vue
+++ src/components/pages/Pricing.vue
@@ -109,4 +109,19 @@ export default {
       width: 100%;
     }
   }
+
+  @media (prefers-color-scheme: dark) {
+    .bg-1 {
+      background-color: var(--dark-1);
+    }
+
+    hr {
+      border-color: var(--light);
+    }
+
+    .price-tier-band {
+      color: var(--color-text-alt);
+    }
+  }
+
 </style>
