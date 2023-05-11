0333b9707b7dafa01561c09780fa9e6114ce5b04
Katarina Mursec <katarina.mursec@gmail.com>
2023-05-11 15:57:09 +0200

    fix flex items

--- src/components/pages/Staff.vue
+++ src/components/pages/Staff.vue
@@ -14,13 +14,13 @@ export default {
   <section>
     <h1>{{ $t('workshops.staff.pageTitle') }}</h1>
     <div class="flex-container flex-gap">
-      <div class="flex-item margin-0">
+      <div class="flex-item flex-basis-100 margin-0">
         <!-- <h2>{{ $t('workshops.staff.teachers.names') }}</h2> -->
         <p v-for="(val, i) in messages.workshops.staff.teachers.description">
           {{ $t(`workshops.staff.teachers.description[${i}]`) }}
         </p>
       </div>
-      <div class="flex-item margin-0 flex-container flex-container-column">
+      <div class="flex-item flex-basis-100 margin-0 flex-container flex-container-column">
         <img src="/src/assets/images/teachers.jpg" :alt="$t('workshops.staff.teachers.names')">
       </div>
     </div>
@@ -30,5 +30,9 @@ export default {
 <style scoped>
   img {
     width: 100%;
+  }
+
+  .flex-basis-100 {
+    flex-basis: 100%;
   }
 </style>
