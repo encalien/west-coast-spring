6f89bc6e7e708b833c3eca41c418e36fdc5b0f9d
Katarina <katarina.mursec@gmail.com>
2023-11-06 20:21:35 +0100

    prepare for fr lang

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createI18n } from 'vue-i18n';
 import App from "./App.vue";
 import en from "./i18n/en";
 import si from "./i18n/si";
+import fr from "./i18n/fr";
 import { createStore } from 'vuex';
 
 // Create a new store instance.
@@ -24,7 +25,6 @@ import Staff from "./components/pages/Staff.vue";
 import EventLocation from "./components/pages/EventLocation.vue";
 import NotFound from "./components/pages/NotFound.vue";
 import Schedule from "./components/pages/Schedule.vue";
-import Registration from "./components/pages/Registration.vue";
 import Pricing from "./components/pages/Pricing.vue";
 import TermsAndConditions from "./components/pages/TermsAndConditions.vue";
 import Levels from "./components/pages/Levels.vue";
@@ -49,20 +49,18 @@ library.add(faEnvelope, faFacebookF, faInstagram, faBars)
 const i18n = createI18n({
   locale: 'en',
   fallbackLocale: 'en',
-  messages: { en, si }
-  // something vue-i18n options here ...
+  messages: { en, si, fr }
 })
 
 const routes: any = [
-  { path: '/:lang(en|si)/', component: Home, },
-  { path: '/:lang(en|si)/workshops/teachers', component: Staff, },
-  { path: '/:lang(en|si)/workshops/levels', component: Levels, },
-  { path: '/:lang(en|si)/workshops/schedule', component: Schedule, },
-  { path: '/:lang(en|si)/pricing', component: Pricing, },
-  { path: '/:lang(en|si)/location/slovenia', component: Slovenia, },
-  { path: '/:lang(en|si)/location/venue', component: EventLocation, },
-  //{ path: ':lang(en|si)/registration', component: Registration, },
-  { path: '/:lang(en|si)/terms-and-conditions', component: TermsAndConditions },
+  { path: '/:lang(en|si|fr)/', component: Home, },
+  { path: '/:lang(en|si|fr)/workshops/teachers', component: Staff, },
+  { path: '/:lang(en|si|fr)/workshops/levels', component: Levels, },
+  { path: '/:lang(en|si|fr)/workshops/schedule', component: Schedule, },
+  { path: '/:lang(en|si|fr)/pricing', component: Pricing, },
+  { path: '/:lang(en|si|fr)/location/slovenia', component: Slovenia, },
+  { path: '/:lang(en|si|fr)/location/venue', component: EventLocation, },
+  { path: '/:lang(en|si|fr)/terms-and-conditions', component: TermsAndConditions },
 ]
 
 const router = createRouter({
