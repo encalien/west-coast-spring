ba94cc89145a522d55c6dea5594e99682f66f410
Katarina <katarina.mursec@gmail.com>
2023-11-17 22:38:49 +0100

    change pricing structure, add highlighting current price

--- src/components/pages/Pricing.vue
+++ src/components/pages/Pricing.vue
@@ -4,9 +4,29 @@ import messages from '../../i18n/en';
 export default {
   data() {
     return {
-      messages: messages
+      messages: messages,
+      activeTierIndex: -1
     }
-  }
+  },
+  methods: {
+    setActiveTierIndex: function () {
+      const tierDates = messages.workshops.pricing.passPrices[0].dates as any[]
+      const today = new Date();
+      
+      for (let i = 0; i < tierDates.length; i++) {
+        const tierFrom = new Date(tierDates[i].from);
+        const tierTo = new Date(tierDates[i].to);
+        if (today >= tierFrom && today <= tierTo) {
+          this.activeTierIndex = i;
+          return;
+        }
+      }
+      this.activeTierIndex = -1;
+    }
+  },
+  created () { 
+    this.setActiveTierIndex()
+  },
 }
 </script>
 
@@ -17,9 +37,28 @@ export default {
       {{ $t(`workshops.pricing.description[${i}]`) }}
     </p>
 
+    <div id="passPrices" class="grid-container">
+      <div v-for="(pass, i) in messages.workshops.pricing.passPrices" 
+           class="grid-container grid-row" :class="{'not-mobile': i === 0}">
+        <h3 class="grid-item grid-center">
+          {{ $t(`workshops.pricing.passPrices[${i}].type`) }}
+        </h3>
+        <div v-for="(tier, j) in pass.tiers" 
+             class="grid-item grid-mobile" 
+             :class="{'grid-center': i, 'active': activeTierIndex === j}">
+          <span class="mobile-only align-left" :class="{'not-mobile': i === 0}">
+            {{ $t(`workshops.pricing.passPrices[0].tiers[${j}]`) }}
+          </span>
+          <span class="align-right" :class="{'not-mobile': i === 0}">
+            {{ $t(`workshops.pricing.passPrices[${i}].tiers[${j}]`) }}
+          </span>
+        </div>
+      </div>
+    </div>
+
     <div id="passes" class="flex-container">
       <div v-for="(val, i) in messages.workshops.pricing.passes" class="pass" :class="`bg-${i + 1}`">
-        <div class="price-tier-band">{{ $t('workshops.pricing.priceTier') }}</div>
+        <div class="price-tier-band">{{ $t('workshops.pricing.currentPriceTier') }}</div>
         <h3>{{ $t(`workshops.pricing.passes[${i}].title`) }}</h3>
         <hr>
         <ul>
@@ -34,9 +73,39 @@ export default {
 </template>
 
 <style scoped>
+  #passPrices h3 {
+    margin: 0;
+  }
+
+  .grid-container {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    margin: -0.5rem 0;
+    align-items: end;
+  }
+
+  .grid-row.grid-container {
+    grid-template-columns: 1fr;
+    grid-auto-flow: row;
+    grid-template-rows: 2fr 1fr 1fr 1fr 1fr;
+    margin: 0;
+  }
+
+  .grid-item {
+    padding: 0.5rem;
+  }
+
+  .grid-center {
+    text-align: center;
+  }
+
+  .active {
+    background-color: var(--accent-1);
+  }
+
   #passes {
     justify-content: space-between;
     gap: 2rem;
+    margin-top: 2rem;
   }
 
   .pass {
@@ -105,6 +174,31 @@ export default {
     .pass {
       flex: none;
       width: 100%;
+    }
+
+    .grid-container {
+      grid-template-columns: 1fr;
+    }
+
+    .grid-container.grid-row {
+      align-items: end;
+    }
+
+    .grid-item.grid-mobile {
+      display: grid;
+      grid-template-columns: 2fr 1fr;
+    }
+
+    .align-left {
+      text-align: left;
+    }
+
+    .align-right {
+      text-align: right;
+    }
+
+    .not-mobile {
+      display: none;
     }
   }
 
