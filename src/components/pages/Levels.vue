ffc2558f6b28a780c5be98a1fcc16414c9b9684c
Katarina <katarina.mursec@gmail.com>
2023-11-17 22:39:13 +0100

    split schedule and levels page

---
+++ src/components/pages/Levels.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import messages from '../../i18n/en';
+
+export default {
+  data() {
+    return {
+      messages: messages
+    }
+  }
+}
+</script>
+
+<template>
+  <section>
+    <h1>{{ $t('workshops.level.pageTitle') }}</h1>
+    <div v-for="(level, i) in messages.workshops.levels.levels" class="flex-item flex-basis-100 margin-0">
+      <h2>{{ $t(`workshops.level.levels[${i}].title`) }}</h2>
+      <p v-for="(p, j) in level.descriptionText">
+        {{ $t(`workshops.level.levels[${i}].descriptionText[${j}]`) }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
--- src/components/pages/Schedule.vue
+++ src/components/pages/Schedule.vue
@@ -15,27 +15,10 @@ export default {
     <h1>{{ $t('workshops.schedule.pageTitle') }}</h1>
     <p>{{ $t('workshops.schedule.description') }}</p>
 
-    <div id="schedule-table" class="grid-container">
-      <div v-for="(val, i) in messages.workshops.schedule.days" class="grid-container grid-gap">
-        <h3 class="grid-row">{{ $t(`workshops.schedule.days[${i}].title`) }}</h3>
-        <div v-for="(val, j) in messages.workshops.schedule.days[i].items" class="grid-row grid-container">
-          <div class="grid-row mobile-small">{{ $t(`workshops.schedule.days[${i}].items[${j}].time`) }}</div>
-          <div class="grid-row" :class="$t(`workshops.schedule.days[${i}].items[${j}].class`)">
-            {{ $t(`workshops.schedule.days[${i}].items[${j}].topic`) }}<br>
-            <small>{{ $t(`workshops.schedule.days[${i}].items[${j}].description`) }}</small>
-          </div>
-        </div>
-        <!-- <hr> -->
-      </div>
-    </div>
-
+    <a :href="$t('workshops.schedule.href')" alt="Schedule">
+      <img :src="$t('workshops.schedule.src')">
+    </a>
   </section>  
-  <section>
-    <h1>{{ $t('workshops.level.title') }}</h1>
-    <p v-for="(val, i) in messages.workshops.level.descriptionText">
-      {{ $t(`workshops.level.descriptionText[${i}]`) }}
-    </p>
-  </section>
 </template>
 
 <style scoped>
